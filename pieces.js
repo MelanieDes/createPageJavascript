922d95f7863d59568580b5b201963307bcaca68f
MelanieDes <125658848+MelanieDes@users.noreply.github.com>
2023-07-06 16:29:03 +0200

    Add files via upload

---
+++ pieces.js
@@ -0,0 +1,116 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+
+for (let i = 0; i < pieces.length; i++) {
+
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+
+ }
+
+ const boutonTrier = document.querySelector(".btn-trier");
+ 
+ boutonTrier.addEventListener("click", () => {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function(a, b) {
+        return a.prix - b.prix;
+    });
+ });
+
+ const boutonTrier2 = document.querySelector(".btn-trier2");
+ 
+ boutonTrier2.addEventListener("click", () => {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function(a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(piecesOrdonnees);
+ });
+
+ const boutonFiltrer = document.querySelector(".btn-filtrer");
+ 
+ boutonFiltrer.addEventListener("click", () => {
+    const piecesFiltres = pieces.filter(function(piece) {
+        return piece.prix >= 35;
+    });
+    console.log(piecesFiltres);
+ });
+
+ const boutonFiltrer2 = document.querySelector(".btn-filtrer2");
+
+ boutonFiltrer2.addEventListener("click", () => {
+    const piecesFiltres = pieces.filter(function(piece) {
+        return piece.description;
+    });
+    console.log(piecesFiltres);
+ });
+
+ const noms = pieces.map(piece => piece.nom);
+ for(let i = pieces.length -1; i>= 0; i--) {
+    if(pieces[i].prix > 35) {
+        noms.splice(i,1)
+    }
+ }
+ 
+//  Création de la liste
+ const abordablesElement = document.createElement('ul');
+
+//  Ajout de chaque nom à la liste
+for(let i=0; i < noms.length; i++) {
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElement.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables').appendChild(abordablesElement);
+
+const nomsDisponibles = pieces.map(piece => piece.nom)
+const prixDisponibles = pieces.map(piece => piece.prix)
+
+for(let i = pieces.length -1; i >= 0; i--) {
+    if(pieces[i].disponibilite === false){
+        nomsDisponibles.splice[i,1]
+        prixDisponibles.splice[i,1]
+    }
+}
+
+const disponiblesElement = document.createElement('ul');
+
+for(let i = 0; i < nomsDisponibles.length; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`
+    disponiblesElement.appendChild(nomElement);
+}
+
+document.querySelector('.disponibles').appendChild(disponiblesElement);
+
+// Efface le contenu de la balise body et donc l'écran
+document.querySelector(".fiches").innerHTML = "";
